c64a5de60371013d882802c0cc6cef3b42e2061b
Alin Secareanu <alin.secareanu@avira.com>
2022-01-17 20:41:04 +0200

    Add toggleterm plugin configuration

--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -1,59 +1,59 @@
-require("toggleterm").setup{
-  -- size can be a number or function which is passed the current terminal
-  -- size = 20 | function(term)
-  --   if term.direction == "horizontal" then
-  --     return 15
-  --   elseif term.direction == "vertical" then
-  --     return vim.o.columns * 0.4
-  --   end
-  -- end,
-  size = 20,
-  -- @TODO: Change this (conflicts with vertical split)
-  open_mapping = [[<c-t>]],
-  -- on_open = fun(t: Terminal), -- function to run when the terminal opens
-  -- on_open = fun(t: Terminal), -- function to run when the terminal opens
-  -- on_close = fun(t: Terminal), -- function to run when the terminal closes
-  hide_numbers = true, -- hide the number column in toggleterm buffers
-  shade_filetypes = {},
-  shade_terminals = true,
-  --shading_factor = '<number>', -- the degree by which to darken to terminal colour, default: 1 for dark backgrounds, 3 for light
-  start_in_insert = true,
-  insert_mappings = true, -- whether or not the open mapping applies in insert mode
-  persist_size = true,
-  --direction = 'vertical' | 'horizontal' | 'window' | 'float',
-  direction = 'float',
-  -- direction = 'horizontal',
-  close_on_exit = true, -- close the terminal window when the process exits
-  shell = vim.o.shell, -- change the default shell
-  -- This field is only relevant if direction is set to 'float'
-  float_opts = {
-    -- The border key is *almost* the same as 'nvim_open_win'
-    -- see :h nvim_open_win for details on borders however
-    -- the 'curved' border is a custom border type
-    -- not natively supported but implemented in this plugin.
-    --border = 'single' | 'double' | 'shadow' | 'curved' | ... other options supported by win open
-    border = 'curved',
-    --width = <value>,
-    --height = <value>,
-    --winblend = 3,
-    highlights = {
-      border = "Normal",
-      background = "Normal",
-    }
-  }
-}
+require("toggleterm").setup({
+	-- size can be a number or function which is passed the current terminal
+	-- size = 20 | function(term)
+	--   if term.direction == "horizontal" then
+	--     return 15
+	--   elseif term.direction == "vertical" then
+	--     return vim.o.columns * 0.4
+	--   end
+	-- end,
+	size = 20,
+	-- @TODO: Change this (conflicts with vertical split)
+	open_mapping = [[<c-\>]],
+	-- on_open = fun(t: Terminal), -- function to run when the terminal opens
+	-- on_open = fun(t: Terminal), -- function to run when the terminal opens
+	-- on_close = fun(t: Terminal), -- function to run when the terminal closes
+	hide_numbers = true, -- hide the number column in toggleterm buffers
+	shade_filetypes = {},
+	shade_terminals = true,
+	--shading_factor = '<number>', -- the degree by which to darken to terminal colour, default: 1 for dark backgrounds, 3 for light
+	start_in_insert = true,
+	insert_mappings = true, -- whether or not the open mapping applies in insert mode
+	persist_size = true,
+	--direction = 'vertical' | 'horizontal' | 'window' | 'float',
+	direction = "float",
+	-- direction = 'horizontal',
+	close_on_exit = true, -- close the terminal window when the process exits
+	shell = vim.o.shell, -- change the default shell
+	-- This field is only relevant if direction is set to 'float'
+	float_opts = {
+		-- The border key is *almost* the same as 'nvim_open_win'
+		-- see :h nvim_open_win for details on borders however
+		-- the 'curved' border is a custom border type
+		-- not natively supported but implemented in this plugin.
+		--border = 'single' | 'double' | 'shadow' | 'curved' | ... other options supported by win open
+		border = "curved",
+		--width = <value>,
+		--height = <value>,
+		--winblend = 3,
+		highlights = {
+			border = "Normal",
+			background = "Normal",
+		},
+	},
+})
 
 function _G.set_terminal_keymaps()
-  local opts = {noremap = true}
-  vim.api.nvim_buf_set_keymap(0, 't', '<esc>', [[<C-t><C-n>]], opts)
-  vim.api.nvim_buf_set_keymap(0, 't', 'jk', [[<C-t><C-n>]], opts)
-  vim.api.nvim_buf_set_keymap(0, 't', '<C-h>', [[<C-t><C-n><C-W>h]], opts)
-  vim.api.nvim_buf_set_keymap(0, 't', '<C-j>', [[<C-t><C-n><C-W>j]], opts)
-  vim.api.nvim_buf_set_keymap(0, 't', '<C-k>', [[<C-t><C-n><C-W>k]], opts)
-  vim.api.nvim_buf_set_keymap(0, 't', '<C-l>', [[<C-t><C-n><C-W>l]], opts)
+	local opts = { noremap = true }
+	vim.api.nvim_buf_set_keymap(0, "t", "<esc>", [[<C-\><C-n>]], opts)
+	vim.api.nvim_buf_set_keymap(0, "t", "jk", [[<C-\><C-n>]], opts)
+	vim.api.nvim_buf_set_keymap(0, "t", "<C-h>", [[<C-\><C-n><C-W>h]], opts)
+	vim.api.nvim_buf_set_keymap(0, "t", "<C-j>", [[<C-\><C-n><C-W>j]], opts)
+	vim.api.nvim_buf_set_keymap(0, "t", "<C-k>", [[<C-\><C-n><C-W>k]], opts)
+	vim.api.nvim_buf_set_keymap(0, "t", "<C-l>", [[<C-\><C-n><C-W>l]], opts)
 end
 
-vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
+vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
 
 local Terminal = require("toggleterm.terminal").Terminal
 local htop = Terminal:new({ cmd = "htop", hidden = true })
