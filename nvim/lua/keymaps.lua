a6f9c215f9728d417ca3051a1ae2465439b4fa9e
Alin Secareanu <alin.secareanu@avira.com>
2022-01-19 15:35:36 +0200

    Format keymaps and add keymappings for no highlight and jk/kj to go from insert to normal mode

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -9,34 +9,38 @@ local opts = { noremap = true, silent = true }
 --    term = "t"
 --    command = "c"
 
-keymap('n', '<leader>vr', ':source ~/.config/nvim/init.lua<CR>', opts)
+keymap("n", "<leader>vr", ":source ~/.config/nvim/init.lua<CR>", opts)
+keymap("n", "<leader>h", ":nohls", opts) -- turn off highligting after search
+
+keymap("i", "jk", "<esc>", opts)
+keymap("i", "kj", "<esc>", opts)
 
 -- Keep selection when identing / outdenting in visual mode
-keymap('v', '<', '<gv', {})
-keymap('v', '>', '>gv', {})
+keymap("v", "<", "<gv", {})
+keymap("v", ">", ">gv", {})
 
 -- Navigate buffers
-keymap('n', '<S-l>', ':bn<CR>', opts)
-keymap('n', '<S-h>', ':bp<CR>', opts)
-keymap('n', '<A-S-l>', ':BufferLineMoveNext<CR>', opts)
-keymap('n', '<A-S-h>', ':BufferLineMovePrev<CR>', opts)
-keymap('n', '<c-x>', ':bd<CR>', opts)
+keymap("n", "<S-l>", ":bn<CR>", opts)
+keymap("n", "<S-h>", ":bp<CR>", opts)
+keymap("n", "<A-S-l>", ":BufferLineMoveNext<CR>", opts)
+keymap("n", "<A-S-h>", ":BufferLineMovePrev<CR>", opts)
+keymap("n", "<c-x>", ":bd<CR>", opts)
 
 -- Navigate windows
-keymap('n', '<A-h>', ':wincmd h<CR>', opts)
-keymap('n', '<A-l>', ':wincmd l<CR>', opts)
-keymap('n', '<A-j>', ':wincmd j<CR>', opts)
-keymap('n', '<A-k>', ':wincmd k<CR>', opts)
+keymap("n", "<A-h>", ":wincmd h<CR>", opts)
+keymap("n", "<A-l>", ":wincmd l<CR>", opts)
+keymap("n", "<A-j>", ":wincmd j<CR>", opts)
+keymap("n", "<A-k>", ":wincmd k<CR>", opts)
 
 -- Window splitting (same keys work in Telescope and NvimTree)
-keymap('n', '<c-v>', '<c-w>v', opts)
-keymap('n', '<c-h>', '<c-w>s' , opts)
+keymap("n", "<c-v>", "<c-w>v", opts)
+keymap("n", "<c-h>", "<c-w>s", opts)
 
 -- Resize with arrows
-keymap('n', '<C-Up>', ':resize -2<CR>', opts)
-keymap('n', '<C-Down>', ':resize +2<CR>', opts)
-keymap('n', '<C-Left>', ':vertical resize -2<CR>', opts)
-keymap('n', '<C-Right>', ':vertical resize +2<CR>', opts)
+keymap("n", "<C-Up>", ":resize -2<CR>", opts)
+keymap("n", "<C-Down>", ":resize +2<CR>", opts)
+keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
+keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 
 -- Move text up and down in visual and visual block mode
 keymap("v", "<A-j>", ":m .+1<CR>==", opts)
@@ -48,12 +52,11 @@ keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
 keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
 
 -- go back / go next cursor position
-keymap('n', '<c-b>', '<c-o>', opts)
-keymap('n', '<c-n>', '<c-i>', opts)
+keymap("n", "<c-b>", "<c-o>", opts)
+keymap("n", "<c-n>", "<c-i>", opts)
 
 --keymap('n', '<c-e>', ':Telescope file_browser<CR>', opts)
-keymap('n', '<c-e>', ':NvimTreeToggle<CR>', opts)
-keymap('n', '<c-o>', ':Telescope find_files<CR>', opts)
+keymap("n", "<c-e>", ":NvimTreeToggle<CR>", opts)
+keymap("n", "<c-o>", ":Telescope find_files<CR>", opts)
 --keymap('n', '<c-f>', ':Telescope grep_string<CR>', opts)
-keymap('n', '<c-f>', ':Telescope current_buffer_fuzzy_find<CR>', opts)
-
+keymap("n", "<c-f>", ":Telescope current_buffer_fuzzy_find<CR>", opts)
