97bc8c2e704b8747b5c4d9375cb575522c939ed3
Alin Secareanu <alin.secareanu@avira.com>
2022-01-27 21:15:16 +0200

    Change nvim theme to onedark with warm style

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,25 +1,25 @@
--- local theme = require("onedark")
-local theme = require("nightfox")
--- theme.setup{
---   style = "warm"
--- }
-theme.setup({
-  fox = "nordfox", -- change the colorscheme to use nordfox
-  styles = {
-    comments = "italic", -- change style of comments to be italic
-    keywords = "bold", -- change style of keywords to be bold
-    functions = "italic,bold" -- styles can be a comma separated list
-  },
-  inverse = {
-    match_paren = true, -- inverse the highlighting of match_parens
-  },
-  colors = {
-    red = "#FF000", -- Override the red color for MAX POWER
-    bg_alt = "#000000",
-  },
-  hlgroups = {
-    TSPunctDelimiter = { fg = "${red}" }, -- Override a highlight group with the color red
-    LspCodeLens = { bg = "#000000", style = "italic" },
-  }
-})
+local theme = require("onedark")
+-- local theme = require("nightfox")
+theme.setup{
+  style = "warm"
+}
+-- theme.setup({
+--   fox = "duskfox",  -- change the colorscheme to use nordfox
+--   styles = {
+--     comments = "italic", -- change style of comments to be italic
+--     keywords = "bold", -- change style of keywords to be bold
+--     functions = "italic,bold" -- styles can be a comma separated list
+--   },
+--   inverse = {
+--     match_paren = true, -- inverse the highlighting of match_parens
+--   },
+--   colors = {
+--     red = "#FF000", -- Override the red color for MAX POWER
+--     bg_alt = "#000000",
+--   },
+--   hlgroups = {
+--     TSPunctDelimiter = { fg = "${red}" }, -- Override a highlight group with the color red
+--     LspCodeLens = { bg = "#000000", style = "italic" },
+--   }
+-- })
 theme.load()
